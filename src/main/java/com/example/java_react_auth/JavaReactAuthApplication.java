4a14cf63b7fe39f751b4d5a27da1932b2e7531d8
Ameya Chandrashekhar Thakur <82252968+Ameya21885@users.noreply.github.com>
2026-04-09 23:51:36 +0530

    adding application properties for prod, dev and main

--- src/main/java/com/example/java_react_auth/JavaReactAuthApplication.java
+++ src/main/java/com/example/java_react_auth/JavaReactAuthApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JavaReactAuthApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JavaReactAuthApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(JavaReactAuthApplication.class, args);
+    }
 
 }
