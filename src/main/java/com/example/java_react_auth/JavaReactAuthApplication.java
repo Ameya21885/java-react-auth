5f658dab13f763238cf0c56c63992d1b56e372de
Ameya Chandrashekhar Thakur <82252968+Ameya21885@users.noreply.github.com>
2026-04-10 22:01:43 +0530

    deploying

--- src/main/java/com/example/java_react_auth/JavaReactAuthApplication.java
+++ src/main/java/com/example/java_react_auth/JavaReactAuthApplication.java
@@ -1,9 +1,11 @@
 package com.example.java_react_auth;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
+// testing
 @SpringBootApplication
 public class JavaReactAuthApplication {
 
@@ -12,8 +14,8 @@ public class JavaReactAuthApplication {
                 .ignoreIfMissing()
                 .load();
 
-        dotenv.entries().forEach(entry ->
-                System.setProperty(entry.getKey(), entry.getValue())
+        dotenv.entries().forEach(entry
+                -> System.setProperty(entry.getKey(), entry.getValue())
         );
 
         SpringApplication.run(JavaReactAuthApplication.class, args);
